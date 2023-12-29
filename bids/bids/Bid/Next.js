ea4a215d06e8965e01a698e8a3a96b02b621da6b
IceOlaf <iceolaf0101@gmail.com>
2023-12-29 00:44:45 -0800

    1

---
+++ bids/bids/Bid/Next.js
@@ -0,0 +1,29 @@
+Dear Sir.
+Thank you for your opportunity.
+I really apperciate for giving me a chance to talk to you.
+Don’t waste your time on other freelancers or auto bidders.
+I am a dedicated and talended Full stack developer focued on frontend development with more than 6+ years of experience. 
+I am a CTO of small company including 10 Full stack developers.
+Our team have a deep knowledge of your work(Next.js,Figma design...) and  very familiar with remote work.
+Expertise is in providing high-quality Drag and Drop User-Friendly UI/Material Design and coding in a very short amount of time.
+Especially, I have 3 years experience at team leader, have experience with requirements collection from client then create plan for team, suggest good features for products, control the logic of system and manage a team.
+I saw your upwork history and I think you are an excellent client.
+So I am sure if I work with you, your application will be an excellent one.
+If you hire me, I can do your job more greatly based on my experience.
+My Skills:
+- Languages: JavaScript, React .js Next.js,Nuxt.js,Node.js,TypeScript, ES6, C/C++, PHP, Python, Rust, golang
+- Frameworks: React, Next, Vue, Nuxt, Angular, Django, Express
+- Blockchain: ERC20/721/1155, Several APIs, Solidity, Vyper
+- Version Management: git, Jira, Trello
+I can start work immediately.
+I sincerly hope that you will take a minute to check about my bid.
+Looking forward your reply.
+Let's discuss the spcificaitons via video call.
+
+☆ Guarantee ☆
+✔ 24/7
+✔ Good communication
+✔ Fully secured
+✔ high-quality
+
+Thank you
---
+++ bids/bids/Bid/Next1.js
@@ -0,0 +1,21 @@
+Dear Sir
+Thank you for your opportunity.
+I just read your job posting and it sounds like you need a full stack(MERN,MEVN) developer.
+I saw your upwork history and I think you are an excellent client.
+So I am sure if I work with you, your application will be an excellent one.
+I’d be curious to hear if you’ve tried explain your design.
+I recently implemented that with another client and the result was http://crio.do.
+I am a dedicated and talended Next.js/React.js developer with more than 5+ years of experience. 
+
+
+My Skills:
+- Languages: JavaScript, Node.js,TypeScript, ES6, C/C++, PHP, Python, Rust, golang
+- Frameworks: React, Next, Vue, Nuxt, Angular, Django, Express
+- Blockchain: ERC20/721/1155, Several APIs, Solidity, Vyper
+- Version Management: git, Jira, Trello
+
+If it seems like a good fit, we’ll schedule a call to talk.
+I would be happy to assist you, as I am practically online 24/7.
+I sincerly hope that you will take a minute to check about my bid.
+I will be never disappointed.
+Looking forward your reply.
